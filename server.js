7b383de442dbc8887ac32aa278b7de3bb4939eab
Reuben Puketapu <reuben.puketapu@gmail.com>
2017-01-18 14:28:01 +1300

    IDK

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ http.createServer(function (req, res) {
 
     if(req.url.indexOf('.html') != -1){ //req.url has the pathname, check if it conatins '.html'
 
-      fs.readFile('index.html', function (err, data) {
+      fs.readFile(__dirname + '/index.html', function (err, data) {
         if (err) console.log(err);
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.write(data);
@@ -16,7 +16,7 @@ http.createServer(function (req, res) {
 
     if(req.url.indexOf('.js') != -1){ //req.url has the pathname, check if it conatins '.js'
 
-      fs.readFile('js/index.js', function (err, data) {
+      fs.readFile(__dirname + '/js/index.js', function (err, data) {
         if (err) console.log(err);
         res.writeHead(200, {'Content-Type': 'text/javascript'});
         res.write(data);
@@ -27,7 +27,7 @@ http.createServer(function (req, res) {
 
     if(req.url.indexOf('.css') != -1){ //req.url has the pathname, check if it conatins '.css'
 
-      fs.readFile('css/style.css', function (err, data) {
+      fs.readFile(__dirname + 'css/style.css', function (err, data) {
         if (err) console.log(err);
         res.writeHead(200, {'Content-Type': 'text/css'});
         res.write(data);
@@ -37,3 +37,4 @@ http.createServer(function (req, res) {
     }
 
 }).listen(80, '0.0.0.0');
+console.log('Server running at http://127.0.0.1:1337/');
