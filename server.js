986f786b7e22110234f17c51e1104381cf35d175
Reuben Puketapu <reuben.puketapu@gmail.com>
2017-01-18 14:13:25 +1300

    added new server.js

--- server.js
+++ server.js
@@ -1,14 +1,40 @@
 var http = require('http'),
-	    fs = require('fs');
+    fs = require('fs');
 
+http.createServer(function (req, res) {
 
-fs.readFile('./index.html', function (err, html) {
-	    if (err) {
-		            throw err; 
-		        }       
-	    http.createServer(function(request, response) {  
-		            response.writeHeader(200, {"Content-Type": "text/html"});  
-		            response.write(html);  
-		            response.end();  
-		        }).listen(80);
-});
+    if(req.url.indexOf('.html') != -1){ //req.url has the pathname, check if it conatins '.html'
+
+      fs.readFile(__dirname + 'index.html', function (err, data) {
+        if (err) console.log(err);
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        res.end();
+      });
+
+    }
+
+    if(req.url.indexOf('.js') != -1){ //req.url has the pathname, check if it conatins '.js'
+
+      fs.readFile(__dirname + 'js/index.js', function (err, data) {
+        if (err) console.log(err);
+        res.writeHead(200, {'Content-Type': 'text/javascript'});
+        res.write(data);
+        res.end();
+      });
+
+    }
+
+    if(req.url.indexOf('.sass') != -1){ //req.url has the pathname, check if it conatins '.css'
+
+      fs.readFile(__dirname + 'style.sass', function (err, data) {
+        if (err) console.log(err);
+        res.writeHead(200, {'Content-Type': 'text/x-sass'});
+        res.write(data);
+        res.end();
+      });
+
+    }
+
+}).listen(80);
+console.log('Server running at http://127.0.0.1:80');
