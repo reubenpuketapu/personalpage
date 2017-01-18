867d4c34df22a9c900145a4eb14068f720845b34
Reuben Puketapu <reuben.puketapu@gmail.com>
2017-01-18 14:21:17 +1300

    fixed

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ http.createServer(function (req, res) {
 
     if(req.url.indexOf('.html') != -1){ //req.url has the pathname, check if it conatins '.html'
 
-      fs.readFile(__dirname + 'index.html', function (err, data) {
+      fs.readFile('index.html', function (err, data) {
         if (err) console.log(err);
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.write(data);
@@ -16,7 +16,7 @@ http.createServer(function (req, res) {
 
     if(req.url.indexOf('.js') != -1){ //req.url has the pathname, check if it conatins '.js'
 
-      fs.readFile(__dirname + 'js/index.js', function (err, data) {
+      fs.readFile('js/index.js', function (err, data) {
         if (err) console.log(err);
         res.writeHead(200, {'Content-Type': 'text/javascript'});
         res.write(data);
@@ -25,15 +25,15 @@ http.createServer(function (req, res) {
 
     }
 
-    // if(req.url.indexOf('.sass') != -1){ //req.url has the pathname, check if it conatins '.css'
+    if(req.url.indexOf('.css') != -1){ //req.url has the pathname, check if it conatins '.css'
 
-    //   fs.readFile(__dirname + 'style.sass', function (err, data) {
-    //     if (err) console.log(err);
-    //     res.writeHead(200, {'Content-Type': 'text/x-sass'});
-    //     res.write(data);
-    //     res.end();
-    //   });
+      fs.readFile('css/style.css', function (err, data) {
+        if (err) console.log(err);
+        res.writeHead(200, {'Content-Type': 'text/css'});
+        res.write(data);
+        res.end();
+      });
 
-    // }
+    }
 
 }).listen(80);
