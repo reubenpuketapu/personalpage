040c94ef6bc5e9f29dc8df3112ffb6d9c1baa575
Reuben Puketapu <reuben.puketapu@gmail.com>
2017-01-18 14:35:17 +1300

    express

--- server.js
+++ server.js
@@ -1,40 +1,6 @@
-var http = require('http'),
-    fs = require('fs');
+var express = require('express');
+var app = express();
 
-http.createServer(function (req, res) {
+app.use(express.static(__dirname + '/public'));
 
-    if(req.url.indexOf('.html') != -1){ //req.url has the pathname, check if it conatins '.html'
-
-      fs.readFile(__dirname + '/index.html', function (err, data) {
-        if (err) console.log(err);
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data);
-        res.end();
-      });
-
-    }
-
-    if(req.url.indexOf('.js') != -1){ //req.url has the pathname, check if it conatins '.js'
-
-      fs.readFile(__dirname + '/js/index.js', function (err, data) {
-        if (err) console.log(err);
-        res.writeHead(200, {'Content-Type': 'text/javascript'});
-        res.write(data);
-        res.end();
-      });
-
-    }
-
-    if(req.url.indexOf('.css') != -1){ //req.url has the pathname, check if it conatins '.css'
-
-      fs.readFile(__dirname + 'css/style.css', function (err, data) {
-        if (err) console.log(err);
-        res.writeHead(200, {'Content-Type': 'text/css'});
-        res.write(data);
-        res.end();
-      });
-
-    }
-
-}).listen(80, '0.0.0.0');
-console.log('Server running at http://127.0.0.1:1337/');
+app.listen(80);
